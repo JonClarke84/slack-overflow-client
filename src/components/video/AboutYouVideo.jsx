a33fedfa3778ab75adc4d133fc59356d77d574aa
Jon Clarke <jonclarke1984@googlemail.com>
2022-05-16 17:19:32 +0100

    first video implemented

--- src/components/video/AboutYouVideo.jsx
+++ src/components/video/AboutYouVideo.jsx
@@ -1,10 +1,31 @@
-import { useVideoConfig, useCurrentFrame, Img } from 'remotion'
+import { useVideoConfig, useCurrentFrame, Img, spring, interpolate } from 'remotion'
 
-export const AboutYouVideo = ({ currentUser, popIn }) => {
+export const AboutYouVideo = ({ currentUser }) => {
 	const { displayName, icon } = currentUser
 	const { fps, durationInFrames, width, height } = useVideoConfig()
 	const frame = useCurrentFrame()
-	const textFade = frame / (durationInFrames / 2)
+
+	const scale = spring({
+		frame: frame - 15,
+		fps,
+		from: 0,
+		to: 1,
+		config: {
+			stiffness: 70,
+		}
+	})
+
+	const textSpring = spring({
+		frame: frame - 30,
+		fps,
+		from: 75,
+		to: 0,
+		config: {
+			stiffness: 100,
+			overshootClamping: true,
+			damping: 50,
+		}
+	})
 
 	return (
 		<div
@@ -25,14 +46,21 @@ export const AboutYouVideo = ({ currentUser, popIn }) => {
 					alignItems: 'center',
 					justifyContent: 'center',
 					color: '#232323',
-					opacity: textFade,
 				}}
 				>
 
-			<Img src={icon} style={{borderRadius: '50%',}}/>	
-
+			<Img src={icon} style={{ borderRadius: '50%',
+															 borderWidth: 5,
+															 borderColour: 'black',
+															 transform: `scale(${scale})`,
+															 }}/>	
+			
+			<div style={{
+				transform: `translateY(${textSpring}px)`,
+			}}>
 			<p>Hello {displayName}!</p>
 			<p>This is your video, it is {durationInFrames/fps} seconds long!</p>
+			</div>
 			</div>
 		</div>
 	)
